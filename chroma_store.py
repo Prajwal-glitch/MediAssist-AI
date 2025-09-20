5186cf0325a7132d6afec690286eced592f69631
Prajwal Patil <73753614+Prajwal-glitch@users.noreply.github.com>
2025-09-20 17:01:23 +0530

    Update chroma_store.py

--- chroma_store.py
+++ chroma_store.py
@@ -2,6 +2,17 @@
 import chromadb
 from pubmed import PubMedRetriever
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 class ChromaStore:
 
     def __init__(self,path="chromadb",name="Intermittent_Fasting"):
